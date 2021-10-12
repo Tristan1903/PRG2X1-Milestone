ff8b3199e018dd6bc037b7c2371ba4cdb1ce729c
Tristan <tristandutoit311@yahoo.com>
2021-10-12 12:59:27 +0200

    Changed Startup Form

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace PRG2X1_Milestone
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Display());
+            Application.Run(new Welcome());
         }
     }
 }
